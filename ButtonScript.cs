a3899565dd2a9fb1be4d75a7ac2b0e0f9ec15c6d
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2023-05-05 18:00:53 +0100

    Button script
    A simple script that moves the door when weight is put on the button. One of the key things that should have been added was a speed value, that would've help for reusability in different situations and testing purposes.

---
+++ ButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    GameObject door;
+    bool active = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        door = GameObject.Find("Door");
+        if (door.transform.position.y < 2 && active == false || door.transform.position.y < -2.5)
+        {
+            door.transform.Translate(Vector3.up * 10 * Time.fixedDeltaTime, Space.World);
+        }
+        active = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Entered Button");
+        door.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+        active = true;
+    }
+}
