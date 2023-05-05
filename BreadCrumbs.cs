7337c30a9c441d0673d66a3a94f9fb763bb13780
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2023-05-05 17:55:37 +0100

    Breadcrumbs script
    The script was designed to make it so when the player pressed the space button, the character would drop a object dubbed "breadcrumb" which would act as a weight to get through the level.

---
+++ BreadCrumbs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadCrumbs : MonoBehaviour
+{
+    public GameObject breadcrumb;
+    public DropBread Use;
+
+    void Start()
+    {
+        breadcrumb = GameObject.Find("BreadCrumb");
+        Use = GameObject.Find("BreadCrumb").GetComponent<DropBread>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Use.usage > 0)
+        {
+            Instantiate(breadcrumb, (this.transform.position - this.transform.forward), this.transform.rotation);
+            Use.usage--;
+            StartCoroutine(BreadCrumbsCooldown());
+        }
+
+    }
+
+    IEnumerator BreadCrumbsCooldown()
+    {
+        float cooldown = Use.usage;
+        Use.usage = 0;
+        yield return new WaitForSeconds(5);
+        Use.usage = cooldown;
+    }
+}
