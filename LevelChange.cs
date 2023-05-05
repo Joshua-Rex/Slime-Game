986686b641726f8dd71a819545782126e7d5ab74
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2023-05-05 18:22:24 +0100

    Level change script
    This script was what controlled the new levels being created. A very complicated way that makes me confused how we even got to this outcome. But I don't feel we needed all this for the script, I don't exactly even know what is happening in the update function, but we could have definitely done without it. Leaving just the script which chooses what level we are on. This could have been done in the game master script which we did have, and have it be a function instead that runs when the player collides with the end game object.

---
+++ LevelChange.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour
+{
+    public Startup levelScript;
+    Transform parentObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        parentObj = GameObject.Find("Level 1").transform;
+        for (int idx = 0; idx < parentObj.childCount; idx++)
+        {
+            parentObj.GetChild(idx).gameObject.SetActive(false);
+        }
+        /*
+        parentObj = GameObject.Find("Level 2").transform;
+        for (int idx = 0; idx < parentObj.childCount; idx++)
+        {
+            parentObj.GetChild(idx).gameObject.SetActive(false);
+        }
+        parentObj = GameObject.Find("Level 3").transform;
+        for (int idx = 0; idx < parentObj.childCount; idx++)
+        {
+            parentObj.GetChild(idx).gameObject.SetActive(false);
+        }
+        */
+
+        switch (1)
+        {
+            case 1:
+                parentObj = GameObject.Find("Level 1").transform;
+                for (int idx = 0; idx < parentObj.childCount; idx++)
+                {
+                    parentObj.GetChild(idx).gameObject.SetActive(true);
+                }
+                break;
+                /*
+            case 2:
+                parentObj = GameObject.Find("Level 2").transform;
+                for (int idx = 0; idx < parentObj.childCount; idx++)
+                {
+                    parentObj.GetChild(idx).gameObject.SetActive(true);
+                }
+                break;
+
+            case 3:
+                parentObj = GameObject.Find("Level 3").transform;
+                for (int idx = 0; idx < parentObj.childCount; idx++)
+                {
+                    parentObj.GetChild(idx).gameObject.SetActive(true);
+                }
+                break;
+                */
+            default:
+                // When it reaches this switch statement, have the game win GUI play
+                break;
+
+        }
+    }
+}
