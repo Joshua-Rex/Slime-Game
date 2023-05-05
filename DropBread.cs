bf0a39fb1a011a6b852fd99c3fe55cc059812076
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2023-05-05 18:12:40 +0100

    Drop breadcrumb script
    I'll be honest, looking back this script was completely unnecessary. It was used to make the breadcrumb shrink in size, destroy the breadcrumb after a certain amount of time and tell the player they can place another. But all of these could have been done from the player controller script, all noted for future reference.

---
+++ DropBread.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DropBread : MonoBehaviour
+{
+    public float usage = 3f;
+    private Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new WaitForSeconds(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1, 0.5f, 1));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "SlimeHitBox")
+        {
+            usage++;
+            Destroy(gameObject);
+        }
+    }
+}
+
+// Reference - https://answers.unity.com/questions/1658741/how-to-fire-projectile-in-direction-character-is-f.html
+// User dbChest, answer submitted August 18, 2019
