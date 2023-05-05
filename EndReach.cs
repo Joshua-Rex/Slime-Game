40815b095937f8fab5e4ee7f2c386a3738e3263e
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2023-05-05 18:16:41 +0100

    End reach script
    This just made it so when the player collides with the object at the end of the level, the player moves to the next. Again this could have been merged into the character controller script. Instead of using the collider script of the end object, it could instead use the collider script of the player and check if the object collided with is the end object.

---
+++ EndReach.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndReach : MonoBehaviour
+{
+    public Startup levelScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelScript = GameObject.Find("GameManager").GetComponent<Startup>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        levelScript.levelNum++;
+    }
+}
